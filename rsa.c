759a10d6b6db1863129dca37529c3b6b285fe8ff
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-24 12:42:38 -0700

    1. RSA Factoring Challenge tasks completed

---
+++ rsa.c
@@ -0,0 +1,39 @@
+#include "factor.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: 0 on success, 1 on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	FILE *fptr;
+	size_t count = 0;
+	ssize_t line;
+	char *buffer = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+		return (1);
+	}
+
+	fptr = fopen(argv[1], "r");
+
+	if (fptr == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		return (1);
+	}
+	while ((line = getline(&buffer, &count, fptr)) != -1)
+	{
+		factorize_number(buffer);
+	}
+
+	free(buffer);
+	fclose(fptr);
+	return (0);
+}
