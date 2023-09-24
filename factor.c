dcb7d9778922347a49dfd36ae5ab2a1a96dd1415
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-24 12:39:21 -0700

    0. Factorize all the things! tasks completed

--- factor.c
+++ factor.c
@@ -11,30 +11,30 @@
 
 int main(int argc, char *argv[])
 {
-    FILE *fptr;
-    size_t count = 0;
-    ssize_t line;
-    char *buffer = NULL;
+	FILE *fptr;
+	size_t count = 0;
+	ssize_t line;
+	char *buffer = NULL;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return (1);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+		return (1);
+	}
 
-    fptr = fopen(argv[1], "r");
+	fptr = fopen(argv[1], "r");
 
-    if (fptr == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return (1);
-    }
-    while ((line = getline(&buffer, &count, fptr)) != -1)
-    {
-        factorize_number(buffer);
-    }
-    free(buffer);
-    fclose(fptr);
-    return (0);
+	if (fptr == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		return (1);
+	}
+
+	while ((line = getline(&buffer, &count, fptr)) != -1)
+	{
+		factorize_number(buffer);
+	}
+	free(buffer);
+	fclose(fptr);
+	return (0);
 }
-
