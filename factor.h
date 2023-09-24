6a8755ce4be532fa2a1df6ddf657e17e95e938ba
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-24 12:34:11 -0700

    0. Factorize all the things! tasks completed

---
+++ factor.h
@@ -0,0 +1,14 @@
+#ifndef FACTOR_HEADER
+#define FACTOR_HEADER
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <stdint.h>
+
+int factorize_number(char *buffer);
+
+#endif
